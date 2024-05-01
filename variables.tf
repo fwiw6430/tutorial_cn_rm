84ff077318e030489874d2404f98180dd3eab722
Tsutomu Miyashita <tsutomu.miyashita@oracle.com>
2024-05-01 11:05:17 +0900

    Initial release

--- variables.tf
+++ variables.tf
@@ -44,6 +44,7 @@ variable "sl_params" {
 }
 variable "public_rt_params" {
   description          = "Route Table Params for public subnet"
+  type = map(object({
   display_name       = string
   rt_rules      = list(object({
     description      = string
@@ -51,15 +52,18 @@ variable "public_rt_params" {
     destination_type = string
     target_is_igw    = bool
   }))
+  }))
 }
 variable "private_rt_params" {
   description          = "Route Table Params for private subnet"
+  type = map(object({
   display_name       = string
   rt_rules      = list(object({
     description      = string
     destination      = string
     destination_type = string
     target_is_igw    = bool
+  }))
   }))
 }
 variable "user_name" { 
