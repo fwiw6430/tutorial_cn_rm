68e2fdbec68cc12845015f9a5b9905a5901cfffd
Tsutomu Miyashita <tsutomu.miyashita@oracle.com>
2024-05-01 10:57:31 +0900

    Initial release

--- variables.tf
+++ variables.tf
@@ -44,25 +44,23 @@ variable "sl_params" {
 }
 variable "public_rt_params" {
   description          = "Route Table Params for public subnet"
-  type = map(object({
-    rt_rules      = list(object({
-        description      = string
-        destination      = string
-        destination_type = string
-        target_is_igw    = bool
-    }
-  }
+  display_name       = string
+  rt_rules      = list(object({
+    description      = string
+    destination      = string
+    destination_type = string
+    target_is_igw    = bool
+  }))
 }
 variable "private_rt_params" {
   description          = "Route Table Params for private subnet"
-  type = map(object({
-    rt_rules      = list(object({
-        description      = string
-        destination      = string
-        destination_type = string
-        target_is_igw    = bool
-    }
-  }
+  display_name       = string
+  rt_rules      = list(object({
+    description      = string
+    destination      = string
+    destination_type = string
+    target_is_igw    = bool
+  }))
 }
 variable "user_name" { 
   description          = "User name to be used to access instances via SSH"
