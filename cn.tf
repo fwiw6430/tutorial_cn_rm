3ba52f77bdaaaaa56843efe04a399de393270cdb
Tsutomu Miyashita <tsutomu.miyashita@oracle.com>
2024-05-01 10:08:46 +0900

    Initial release

--- cn.tf
+++ cn.tf
@@ -48,7 +48,7 @@ resource "oci_core_cluster_network" "cn" {
   }
   placement_configuration {
     availability_domain         = var.ad
-    primary_subnet_id           = oci_core_subnet.sub["private"].id
+    primary_subnet_id           = oci_core_subnet.private_sub.id
   }
 }
 
