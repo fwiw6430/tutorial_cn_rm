f4d36664febf0f38ac71a61f4700a965401bccb0
Tsutomu Miyashita <tsutomu.miyashita@oracle.com>
2024-06-18 22:25:20 +0900

    Add new feature for deploying HPC/GPU cluster on pre-existing VCN

--- variables.tf
+++ variables.tf
@@ -1,62 +1,62 @@
-variable "vcn_params" {
-  description          = "VCN Parameters: vcn_cidr, display_name, dns_label"
-  type                 = map(string)
-}
-variable "igw_display_name" {
-  description          = "Internet Gateway display name"
-  type                 = string
-}
-variable "ngw_display_name" {
-  description          = "NAT Gateway display name"
-  type                 = string
-}
-variable "subnet_params" {
-  description          = "Subnet Parameters"
-  type = map(object({
-    display_name       = string
-    cidr_block         = string
-    dns_label          = string
-    is_subnet_private  = bool
-    sl_name            = string
-    rt_name            = string
-  })) 
-}
-variable "sl_params" {
-  description          = "Security List Params"
-  type = map(object({
-    display_name       = string
-    ingress_rules      = list(object({
-      stateless        = string
-      protocol         = string
-      source           = string
-      tcp_options      = list(object({
-        min            = number
-        max            = number
-      }))
-      udp_options      = list(object({
-        min            = number
-        max            = number
-      }))
-    }))
-    egress_rules       = list(object({
-      stateless        = string
-      protocol         = string
-      destination      = string
-    }))
-  }))
-}
-variable "rt_params" {
-  description          = "Route Table Params"
-  type                 = map(object({
-    display_name       = string
-    rt_rules = list(object({
-      description      = string
-      destination      = string
-      destination_type = string
-      target_is_igw    = bool
-    }))
-  }))
-}
+#variable "vcn_params" {
+#  description          = "VCN Parameters: vcn_cidr, display_name, dns_label"
+#  type                 = map(string)
+#}
+#variable "igw_display_name" {
+#  description          = "Internet Gateway display name"
+#  type                 = string
+#}
+#variable "ngw_display_name" {
+#  description          = "NAT Gateway display name"
+#  type                 = string
+#}
+#variable "subnet_params" {
+#  description          = "Subnet Parameters"
+#  type = map(object({
+#    display_name       = string
+#    cidr_block         = string
+#    dns_label          = string
+#    is_subnet_private  = bool
+#    sl_name            = string
+#    rt_name            = string
+#  })) 
+#}
+#variable "sl_params" {
+#  description          = "Security List Params"
+#  type = map(object({
+#    display_name       = string
+#    ingress_rules      = list(object({
+#      stateless        = string
+#      protocol         = string
+#      source           = string
+#      tcp_options      = list(object({
+#        min            = number
+#        max            = number
+#      }))
+#      udp_options      = list(object({
+#        min            = number
+#        max            = number
+#      }))
+#    }))
+#    egress_rules       = list(object({
+#      stateless        = string
+#      protocol         = string
+#      destination      = string
+#    }))
+#  }))
+#}
+#variable "rt_params" {
+#  description          = "Route Table Params"
+#  type                 = map(object({
+#    display_name       = string
+#    rt_rules = list(object({
+#      description      = string
+#      destination      = string
+#      destination_type = string
+#      target_is_igw    = bool
+#    }))
+#  }))
+#}
 variable "user_name" { 
   description          = "User name to be used to access instances via SSH"
   type                 = string
